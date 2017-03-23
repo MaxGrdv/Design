cf23f7dd1006adf0e760eaa7fed29a9deac94453
MaxGrdv <zenitlike@gmail.com>
2017-03-23 22:32:40 +0300

    add a button and Menubar instead of label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    public void initialize() {
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> button.setText("Is Done"));
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,34 +3,25 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 
 public class Main extends Application {
     private Stage stage;
     private Scene scene;
     private AnchorPane pane;
-    private Label label;
 
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
-        try {
-            pane = (AnchorPane)FXMLLoader.load(Main.class.getResource("sample.fxml"));
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+
+        pane = (AnchorPane) FXMLLoader.load(Main.class.getResource("sample.fxml"));
         scene = new Scene(pane, 600, 300);
         stage.setScene(scene);
         stage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
